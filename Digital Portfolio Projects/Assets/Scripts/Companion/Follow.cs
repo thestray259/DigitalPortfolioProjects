2ef88c1ec0fa6784c8266873248f6039c7b997cd
Tobie Allen <tallen@student.neumont.edu>
2023-04-10 11:48:16 -0600

    started on updating companion follow

--- Digital Portfolio Projects/Assets/Scripts/Companion/Follow.cs
+++ Digital Portfolio Projects/Assets/Scripts/Companion/Follow.cs
@@ -22,12 +22,23 @@ public class Follow : Node
     public override NodeState Evaluate()
     {
         Debug.Log("Companion entered Follow");
+        Vector3 destination = new Vector3();
+
+        Debug.Log("Walking Bool: " + animator.GetBool("walking"));         
+
         if (Vector3.Distance(transform.position, playerTransform.position) > 3.0f)
         {
+            //if (animator.GetBool("walking") == false)
+            {
+                Quaternion rotation = Quaternion.AngleAxis(Random.Range(-45, 45), Vector3.up);
+                Vector3 forward = rotation * Vector3.forward;
+                destination = transform.position + forward * Random.Range(3f, 5);
+            }
+
             animator.SetBool("walking", true);
             // change so that comp picks a position behind player to move towards
-            transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, CompanionBT.speed * Time.deltaTime);
-            transform.LookAt(playerTransform.position);
+            transform.position = Vector3.MoveTowards(transform.position, destination, CompanionBT.speed * Time.deltaTime);
+            transform.LookAt(destination);
         }
         else { animator.SetBool("walking", false); }
 
